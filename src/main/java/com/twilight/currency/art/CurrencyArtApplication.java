295f033d63a8f13bb0c8c621dbe99485163b282b
郭俊 <guojun@douyu.tv>
2020-06-10 15:15:50 +0800

    Initial commit

---
+++ src/main/java/com/twilight/currency/art/CurrencyArtApplication.java
@@ -0,0 +1,13 @@
+package com.twilight.currency.art;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyArtApplication.class, args);
+	}
+
+}
